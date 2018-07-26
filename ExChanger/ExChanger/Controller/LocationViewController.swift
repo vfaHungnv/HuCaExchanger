5a18fa1e3738466723c99cf4776135e4ceb4a15f
HungNV <hungnv@vitalify.asia>
2018-07-26 17:53:50 +0700

    #first commited.

---
+++ ExChanger/ExChanger/Controller/LocationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LocationViewController.swift
+//  ExChanger
+//
+//  Created by HungNV on 7/26/18.
+//  Copyright © 2018 HungNV. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
